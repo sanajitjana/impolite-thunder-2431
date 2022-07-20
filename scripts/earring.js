183061168704d0899ef428f9f4f5ef1a1e88fddc
Shivani Pandey <104144826+shivanipandey22@users.noreply.github.com>
2022-07-21 00:55:09 +0530

    earrings data appended

---
+++ scripts/earring.js
@@ -0,0 +1,358 @@
+var earrings =[
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000098-3__52395_360x.jpg?v=1645115762",
+        head:"Ray Of Light Hoops",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101015-3_360x.jpg?v=1646760639",
+        head:"Daisy Post Earrings",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011300001-3__08167_360x.jpg?v=1645110216",
+        head:"Maryland Crab Earrings (18K & 24K)",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101044-3_360x.jpg?v=1646760990",
+        head:"Fir Drop Earrings",
+        price:"25",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101035-4_360x.jpg?v=1647540161",
+        head:"Cyprus Hoop Earrings",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101047-C-4_360x.jpg?v=1647541449",
+        head:"Classique Hoop Earrings|Amber",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101046-4_360x.jpg?v=1647540212",
+        head:"Vault Huggie Hoop Earrings ",
+        price:"25",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101028-4_360x.jpg?v=1647540053",
+        head:"Vinci Ear Jacket",
+        price:"21",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010200810-3__10374_360x.jpg?v=1645116095",
+        head:"Oval Wicker Drop Earrings",
+        price:"15",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101005-3__50209_360x.jpg?v=1645116078",
+        head:"Simplicity Hoop Earrings",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010200826-C-3__07211_360x.jpg?v=1652369565",
+        head:"Rhinestone Wing Earrings",
+        price:"14",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000124-5__81084_360x.jpg?v=1645115771",
+        head:"Ginkgo Leaf Hoop Earrings (14K)",
+        price:"17",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010100535-4__11921_360x.jpg?v=1645114232",
+        head:"Boss Status Bar Earrings (14K)",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101026-B-4_360x.jpg?v=1649869699",
+        head:"Fraise Dangle Earrings",
+        price:"26",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101053-4_360x.jpg?v=1647606502",
+        head:"Flare Post Earrings",
+        price:"32",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101051-4_360x.jpg?v=1647606141",
+        head:"Astoria Hook Earrings",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101047-B-4_360x.jpg?v=1647541344",
+        head:"Classique Hoop Earrings | Cream",
+        price:"19",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101006-3__61677_360x.jpg?v=1645116374",
+        head:"Canal Hoop Earrings",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101002-3__54691_360x.jpg?v=1645116369",
+        head:"Northern Star Hoop Earrings",
+        price:"21",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101000-3__13840_360x.jpg?v=1645116343",
+        head:"Esmeralda Hoop Earrings",
+        price:"21",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000158-3__78715_360x.jpg?v=1645116065",
+        head:"Beaded Oval Hoop Earrings",
+        price:"30",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000153-3__43681_360x.jpg?v=1645115891",
+        head:"Chase The Rainbow Earrings",
+        price:"14",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010200823-3__99867_360x.jpg?v=1645116314",
+        head:"Double the Gold Ear Cuff (14K)",
+        price:"17",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010700034-3__78314_360x.jpg?v=1645115734",
+        head:"Tropical Hoop Earrings (14K)",
+        price:"14",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000116-5__37345_360x.jpg?v=1645115443",
+        head:"Single Stone Stud Earrings(14K)",
+        price:"11",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010100499-m_1_of_1__84652_360x.jpg?v=1645113693",
+        head:"Straight Bar Ear Crawlers(15K)",
+        price:"15",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010400034-4__16790_360x.jpg?v=1645112193",
+        head:"Congruent Link Earrings",
+        price:"12",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010200638-3__10103_360x.jpg?v=1645114554",
+        head:"Rene Drop Earrings",
+        price:"21",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101049-4_360x.jpg?v=1647606605",
+        head:"Starfish Stud Earrings",
+        price:"21",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101022-4_360x.jpg?v=1646421025",
+        head:"Heiress Huggie Hoop Earrings",
+        price:"18",
+    },
+]
+
+var necklaces=[
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201013-3__21594_360x.jpg?v=1645115943",
+        head:"Plate Oval Chain Necklace",
+        price:"17",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201000-3__93474_360x.jpg?v=1645116355",
+        head:"Samba Charm Necklace",
+        price:"24",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100463-3__29850_360x.jpg?v=1645112741",
+        head:"Antigone Necklace (14K)",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201011-3__04676_360x.jpg?v=1645116105",
+        head:"Othello Layered Necklace",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201010-3__55417_360x.jpg?v=1645116376",
+        head:"Double Double Necklace",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201006-3__15448_360x.jpg?v=1645116370",
+        head:"Shine Bright-Necklace (14K)",
+        price:"30",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201006-3__15448_360x.jpg?v=1645116370",
+        head:"Ombre Stone Tear Necklace",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/IMG_5090__61478_360x.jpg?v=1645115397",
+        head:"Hamlet Layered Necklace",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020200058__13997_360x.jpg?v=1645113595",
+        head:"Perfect Line Necklace",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020500259-4__26964_360x.jpg?v=1645112654",
+        head:"Modern Minimalist Bar Necklace (14K)",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201005-3__67325_360x.jpg?v=1645115932",
+        head:"Sunset Circle Necklace (14K)",
+        price:"14",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100565-4__69487_360x.jpg?v=1645114961",
+        head:"Horizon Necklace",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100562-4__94244_360x.jpg?v=1645115404",
+        head:"Diamonds Are Forever Necklace",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100369-3__28478_360x.jpg?v=1645111643",
+        head:"Cassio Charm Necklace",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020200068-5__58390_360x.jpg?v=1645114243",
+        head:"Helio Charm Necklace",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201014-3__28860_360x.jpg?v=1645116382",
+        head:"Snake Chain Necklace",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201009-3__65781_360x.jpg?v=1645115938",
+        head:"Stellar Charm Necklace",
+        price:"20",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201004-B-2__16458_360x.jpg?v=1645116091",
+        head:"Chain Lariat Necklace |Gold(14K)",
+        price:"24",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201003-3__29983_360x.jpg?v=1645115926",
+        head:"Bubble Chain Choker Necklace ",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201002-3__96915_360x.jpg?v=1645116380",
+        head:"Plain Chain Necklace",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020600156-G-4__61767_360x.jpg?v=1645115807",
+        head:"Sparkling Stone Necklace",
+        price:"14",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020700077-4__96840_360x.jpg?v=1645116261",
+        head:"Rumi Stone Necklace",
+        price:"20",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100576-3__24975_360x.jpg?v=1645116095",
+        head:"Pixum Compass Stone Necklace",
+        price:"20",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100577-1__75323_360x.jpg?v=1645116023",
+        head:"Laguna Pearl Necklace",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100564-4__22201_360x.jpg?v=1645115344",
+        head:"Gold Beaded Chain Necklace",
+        price:"18",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020500295-3__24966_360x.jpg?v=1645113006",
+        head:"Once Upon a Chain Necklace",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020500158-2__28336_360x.jpg?v=1645111186",
+        head:"Overawe Large Chain Link Necklace(14K)",
+        price:"34",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020600050-3__52178_360x.jpg?v=1648824851",
+        head:"La Vie Necklace (14K)",
+        price:"21",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201008-3__26789_360x.jpg?v=1645116393",
+        head:"Blush Bouquet Necklace",
+        price:"16",
+    },
+    {
+        image:"https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020200076-5__52896_360x.jpg?v=1645115621",
+        head:"Petal Pendant Layered Necklace",
+        price:"20",
+    },
+]
+
+
+
+localStorage.setItem("earringData",JSON.stringify(earrings)); 
+
+var earringdata = JSON.parse(localStorage.getItem("earringData"));
+
+console.log(earringdata)
+
+display(earringdata) ;
+
+function display(data) {
+    // console.log(data)
+data.map(function (el) {
+var card = document.createElement("div");
+card.setAttribute("id", "card")
+
+var image = document.createElement("img");
+image.src = el.image;
+
+var head = document.createElement("h5");
+head.textContent = el.head;
+
+var price = document.createElement("p")
+price.textContent = `$ ${el.price}`;
+price.setAttribute("class", "para")
+
+var fav = document.createElement("img")
+fav.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQobfSOCPduYpVTa2kAHaLWapi651J_Sp6yTQ&usqp=CAU"
+fav.setAttribute("class", "symbol1")
+
+
+var div2 = document.createElement("div")
+div2.setAttribute("class", "box2")
+
+
+div2.append(head,price)
+
+card.append(image,div2);
+
+card.addEventListener("click", zoomin)
+function zoomin() {
+    localStorage.setItem("zoomin", JSON.stringify(el))
+    window.location.href = ""
+}
+
+document.querySelector("#earringdiv").append(card)
+fav.addEventListener("click", function () {
+    alert("Product is added to wishlist")
+    
+})
+})
+}

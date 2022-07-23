a21698d5d4dae55fe8ea2d1272b714836ec55707
sanajitjana <dev.sanajitjana@gmail.com>
2022-07-24 00:02:44 +0530

    put id on object array

--- components/shop-all-objects.js
+++ components/shop-all-objects.js
@@ -5,11 +5,12 @@ let shopall = [
     hovimage:
       "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L401001-6-3__11505_823x.jpg?v=1645115949",
     head: "Bubbling Adjustable Ring",
-    price: 14.0,
+    price: 14,
     category: "Rings",
     date: 2022,
     tag: "Bestselling",
     availability: true,
+    id: 1,
   },
   {
     image:
@@ -17,11 +18,12 @@ let shopall = [
     hovimage:
       "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020500259-4__26964_360x.jpg?v=1645112654",
     head: "Modern Minimalist Bar Necklace (14K)",
-    price: 16.0,
+    price: 16,
     category: "necklaces",
     date: 2022,
     tag: "Featured",
     availability: false,
+    id: 2,
   },
   {
     image:
@@ -29,12 +31,12 @@ let shopall = [
     hovimage:
       "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-C-4_360x.jpg?v=1650308512",
     head: "Textile Drop Earrings | Florals",
-    price: 25.0,
+    price: 25,
     category: "New",
     date: 2019,
     tag: "Bestselling",
     availability: true,
-    id: 6,
+    id: 3,
   },
   {
     image:
@@ -42,69 +44,7 @@ let shopall = [
     hovimage:
       "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101061-4_360x.jpg?v=1650308606",
     head: "Terrace Drop Earrings",
-    price: 25.0,
-    category: "New",
-    date: 2018,
-    tag: "Bestselling",
-    availability: true,
-    id: 3,
-  },
-  {
-    image:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000124-3__50511_493x.jpg?v=1645115769",
-    hovimage:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000124-5__81084_360x.jpg?v=1645115771",
-    head: "Ginkgo Leaf Hoop Earrings (14K)",
-    price: 17.0,
-    category: "earrings",
-    date: 2021,
-    tag: "bestselling",
-    availability: true,
-  },
-  {
-    image:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-C-1_823x.jpg?v=1650308512",
-    hovimage:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-C-4_360x.jpg?v=1650308512",
-    head: "Textile Drop Earrings | Checkered",
-    price: 25.0,
-    category: "New",
-    date: 2018,
-    tag: "Bestselling",
-    availability: true,
-    id: 5,
-  },
-  {
-    image:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030100157-2__79685_493x.jpg?v=1645115596",
-    hovimage:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030100157-3__78566_493x.jpg?v=1645115598",
-    head: "Truly Chained Bracelet(14K)",
-    price: 11.0,
-    category: "bracelets",
-    date: 2022,
-    tag: "new",
-    availability: true,
-  },
-  {
-    image:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800021-G-1__54877_493x.jpg?v=1645115919",
-    hovimage:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800021-G-3__10223_493x.jpg?v=1645115923",
-    head: "Mod Box Chain Anklet |Gold",
-    price: 23.0,
-    category: "bracelets",
-    date: 2022,
-    tag: "bestselling",
-    availability: true,
-  },
-  {
-    image:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-D-2_823x.jpg?v=1650308562",
-    hovimage:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-D-4_360x.jpg?v=1650308562",
-    head: "Textile Drop Earrings | Houndstooth",
-    price: 25.0,
+    price: 25,
     category: "New",
     date: 2018,
     tag: "Bestselling",
@@ -113,15 +53,81 @@ let shopall = [
   },
   {
     image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000124-3__50511_493x.jpg?v=1645115769",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000124-5__81084_360x.jpg?v=1645115771",
+    head: "Ginkgo Leaf Hoop Earrings (14K)",
+    price: 17,
+    category: "earrings",
+    date: 2021,
+    tag: "bestselling",
+    availability: true,
+    id: 5,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-C-1_823x.jpg?v=1650308512",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-C-4_360x.jpg?v=1650308512",
+    head: "Textile Drop Earrings | Checkered",
+    price: 25,
+    category: "New",
+    date: 2018,
+    tag: "Bestselling",
+    availability: true,
+    id: 6,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030100157-2__79685_493x.jpg?v=1645115596",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030100157-3__78566_493x.jpg?v=1645115598",
+    head: "Truly Chained Bracelet(14K)",
+    price: 11,
+    category: "bracelets",
+    date: 2022,
+    tag: "new",
+    availability: true,
+    id: 7,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800021-G-1__54877_493x.jpg?v=1645115919",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800021-G-3__10223_493x.jpg?v=1645115923",
+    head: "Mod Box Chain Anklet |Gold",
+    price: 23,
+    category: "bracelets",
+    date: 2022,
+    tag: "bestselling",
+    availability: true,
+    id: 8,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-D-2_823x.jpg?v=1650308562",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-D-4_360x.jpg?v=1650308562",
+    head: "Textile Drop Earrings | Houndstooth",
+    price: 25,
+    category: "New",
+    date: 2018,
+    tag: "Bestselling",
+    availability: true,
+    id: 9,
+  },
+  {
+    image:
       "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800012-2__19471_493x.jpg?v=1645116158",
     hovimage:
       "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800012-3__94518_493x.jpg?v=1645116160",
     head: "Minimal Circle Rhinestone Anklet",
-    price: 19.0,
+    price: 19,
     category: "bracelets",
     date: 2018,
     tag: "bestselling",
     availability: true,
+    id: 10,
   },
   {
     image:
@@ -129,11 +135,12 @@ let shopall = [
     hovimage:
       "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030100160-3__31041_493x.jpg?v=1645115637",
     head: "Thin and Thin Chain Bracelet(18K)",
-    price: 24.0,
+    price: 24,
     category: "bracelets",
     date: 2021,
     tag: "Featured",
     availability: false,
+    id: 11,
   },
   {
     image:
@@ -141,11 +148,12 @@ let shopall = [
     hovimage:
       "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011300001-3__08167_360x.jpg?v=1645110216",
     head: "Maryland Crab Earrings (18K & 24K)",
-    price: 18.0,
+    price: 18,
     category: "earrings",
     date: 2018,
     tag: "bestselling",
     availability: false,
+    id: 12,
   },
   {
     image:
@@ -153,11 +161,12 @@ let shopall = [
     hovimage:
       "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010200826-C-3__07211_360x.jpg?v=1652369565",
     head: "Rhinestone Wing Earrings",
-    price: 14.0,
+    price: 14,
     category: "earrings",
     date: 2018,
     tag: "bestselling",
     availability: false,
+    id: 13,
   },
   {
     image:
@@ -165,11 +174,12 @@ let shopall = [
     hovimage:
       "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010100535-4__11921_360x.jpg?v=1645114232",
     head: "Boss Status Bar Earrings (14K)",
-    price: 16.0,
+    price: 16,
     category: "earrings",
     date: 2022,
     tag: "Featured",
     availability: false,
+    id: 14,
   },
   {
     image:
@@ -177,12 +187,12 @@ let shopall = [
     hovimage:
       "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L401019-7-3_823x.jpg?v=1645728794",
     head: "Braided Ring",
-    price: 14.0,
+    price: 14,
     category: "Rings",
     date: 2022,
     tag: "Bestselling",
     availability: true,
-    id: 31,
+    id: 15,
   },
   {
     image:
@@ -190,12 +200,12 @@ let shopall = [
     hovimage:
       "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L401010-7-3__37928_823x.jpg?v=1645116399",
     head: "Bridge Ring",
-    price: 14.0,
+    price: 14,
     category: "Rings",
     date: 2022,
     tag: "Bestselling",
     availability: true,
-    id: 32,
+    id: 16,
   },
 ];
 

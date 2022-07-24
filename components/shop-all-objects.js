17c8bfd1d7f521a80d6907542101590fe90abf33
sanajitjana <dev.sanajitjana@gmail.com>
2022-07-25 00:32:48 +0530

    add more data

--- components/shop-all-objects.js
+++ components/shop-all-objects.js
@@ -207,6 +207,522 @@ let ArrayofObject = [
     availability: true,
     id: 16,
   },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000098-3__52395_360x.jpg?v=1645115762",
+    head: "Ray Of Light Hoops",
+    price: 16,
+    tag: "Bestselling",
+    category: "Earrings",
+    date: 2020,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000098-2__43894_493x.jpg?v=1645115759",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101015-3_360x.jpg?v=1646760639",
+    head: "Daisy Post Earrings",
+    price: 16,
+    tag: "Featured",
+    category: "Earrings",
+    date: 2021,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101015-1_493x.jpg?v=1646760637",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011300001-3__08167_360x.jpg?v=1645110216",
+    head: "Maryland Crab Earrings (18K & 24K)",
+    price: 18,
+    tag: "Bestselling",
+    category: "Earrings",
+    date: 2018,
+    availability: false,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011300002-2__23761_493x.jpg?v=1645110224",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101044-3_360x.jpg?v=1646760990",
+    head: "Fir Drop Earrings",
+    price: 25,
+    tag: "Featured",
+    category: "Earrings",
+    date: 2020,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101044-2_493x.jpg?v=1646760990",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101035-4_360x.jpg?v=1647540161",
+    head: "Cyprus Hoop Earrings",
+    price: 18,
+    tag: "Bestselling",
+    category: "Earrings",
+    date: 2021,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101035-1_493x.jpg?v=1647540132",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101047-C-4_360x.jpg?v=1647541449",
+    head: "Classique Hoop Earrings|Amber",
+    price: 16,
+    tag: "Featured",
+    category: "Earrings",
+    date: 2018,
+    availability: false,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101047-C-2_493x.jpg?v=1647541448",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101046-4_360x.jpg?v=1647540212",
+    head: "Vault Huggie Hoop Earrings ",
+    price: 25,
+    tag: "New",
+    category: "Earrings",
+    date: 2020,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101046-1_493x.jpg?v=1647540205",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101028-4_360x.jpg?v=1647540053",
+    head: "Vinci Ear Jacket",
+    price: 21,
+    tag: "Bestselling",
+    category: "Earrings",
+    date: 2021,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101028-2_493x.jpg?v=1647540052",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010200810-3__10374_360x.jpg?v=1645116095",
+    head: "Oval Wicker Drop Earrings",
+    price: 15,
+    tag: "Featured",
+    category: "Earrings",
+    date: 2021,
+    availability: false,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010200810-2__70803_493x.jpg?v=1645116093",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101005-3__50209_360x.jpg?v=1645116078",
+    head: "Simplicity Hoop Earrings",
+    price: 16,
+    tag: "Bestselling",
+    category: "Earrings",
+    date: 2020,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101005-2__93742_493x.jpg?v=1645116076",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010200826-C-3__07211_360x.jpg?v=1652369565",
+    head: "Rhinestone Wing Earrings",
+    price: 14,
+    tag: "Bestselling",
+    category: "Earrings",
+    date: 2018,
+    availability: false,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010200826-C-1__83187_493x.jpg?v=1652369565",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000124-5__81084_360x.jpg?v=1645115771",
+    head: "Ginkgo Leaf Hoop Earrings (14K)",
+    price: 17,
+    tag: "Bestselling",
+    category: "Earrings",
+    date: 2021,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011000124-3__50511_493x.jpg?v=1645115769",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010100535-4__11921_360x.jpg?v=1645114232",
+    tag: "New",
+    head: "Boss Status Bar Earrings (14K)",
+    price: 16,
+    category: "Earrings",
+    date: 2022,
+    availability: false,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010100535-2__75509_493x.jpg?v=1645114230",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101026-B-4_360x.jpg?v=1649869699",
+    head: "Fraise Dangle Earrings",
+    price: 26,
+    tag: "Bestselling",
+    category: "Earrings",
+    date: 2018,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101026-B-1_493x.jpg?v=1649869645",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101053-4_360x.jpg?v=1647606502",
+    head: "Flare Post Earrings",
+    price: 32,
+    tag: "Featured",
+    category: "Earrings",
+    date: 2021,
+    availability: false,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101053-1_493x.jpg?v=1647606494",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101051-4_360x.jpg?v=1647606141",
+    head: "Astoria Hook Earrings",
+    price: 18,
+    tag: "Bestselling",
+    category: "Earrings",
+    date: 2022,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101051-1_493x.jpg?v=1647606138",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201013-3__21594_360x.jpg?v=1645115943",
+    head: "Plate Oval Chain Necklace",
+    price: 17,
+    tag: "Bestselling",
+    category: "Necklaces",
+    date: 2021,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201013-2__98789_493x.jpg?v=1645115942",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201000-3__93474_360x.jpg?v=1645116355",
+    head: "Samba Charm Necklace",
+    price: 24,
+    tag: "Featured",
+    category: "Necklaces",
+    date: 2018,
+    availability: false,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201000-2__49671_493x.jpg?v=1645116352",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100463-3__29850_360x.jpg?v=1645112741",
+    head: "Antigone Necklace (14K)",
+    price: 18,
+    tag: "Bestselling",
+    category: "Necklaces",
+    date: 2021,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100463-1__46464_493x.jpg?v=1645112739",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201011-3__04676_360x.jpg?v=1645116105",
+    head: "Othello Layered Necklace",
+    price: 18,
+    tag: "Featured",
+    category: "Necklaces",
+    date: 2020,
+    availability: false,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201011-2__52444_493x.jpg?v=1645116103",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201010-3__55417_360x.jpg?v=1645116376",
+    head: "Double Double Necklace",
+    price: 16,
+    tag: "Bestselling",
+    category: "Necklaces",
+    date: 2018,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201010-1__26041_493x.jpg?v=1645116374",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201006-3__15448_360x.jpg?v=1645116370",
+    head: "Shine Bright-Necklace (14K)",
+    price: 30,
+    tag: "Featured",
+    category: "Necklaces",
+    date: 2020,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201006-2__28603_493x.jpg?v=1645116368",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201006-3__15448_360x.jpg?v=1645116370",
+    head: "Ombre Stone Tear Necklace",
+    price: 18,
+    tag: "New",
+    category: "Necklaces",
+    date: 2018,
+    availability: false,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020400079-3__32862_493x.jpg?v=1645115394",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/IMG_5090__61478_360x.jpg?v=1645115397",
+    head: "Hamlet Layered Necklace",
+    price: 16,
+    tag: "Bestselling",
+    category: "Necklaces",
+    date: 2022,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100370-2__34904_493x.jpg?v=1645113593",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020200058__13997_360x.jpg?v=1645113595",
+    head: "Perfect Line Necklace",
+    price: 18,
+    tag: "New",
+    category: "Necklaces",
+    date: 2020,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020500259-1__20614_493x.jpg?v=1645112658",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020500259-4__26964_360x.jpg?v=1645112654",
+    head: "Modern Minimalist Bar Necklace (14K)",
+    price: 16,
+    tag: "Featured",
+    category: "Necklaces",
+    date: 2022,
+    availability: false,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201005-2__41179_493x.jpg?v=1645115930",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201005-3__67325_360x.jpg?v=1645115932",
+    head: "Sunset Circle Necklace (14K)",
+    price: 14,
+    tag: "Bestselling",
+    category: "Necklaces",
+    date: 2018,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100575-4__97470_493x.jpg?v=1645115770",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100565-4__69487_360x.jpg?v=1645114961",
+    head: "Horizon Necklace",
+    price: 16,
+    tag: "New",
+    category: "Necklaces",
+    date: 2020,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100565-2__58904_493x.jpg?v=1645114959",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100562-4__94244_360x.jpg?v=1645115404",
+    head: "Diamonds Are Forever Necklace",
+    price: 18,
+    tag: "Bestselling",
+    category: "Necklaces",
+    date: 2022,
+    availability: false,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100562_2__87633_493x.jpg?v=1645115402",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020100369-3__28478_360x.jpg?v=1645111643",
+    head: "Cassio Charm Necklace",
+    price: 18,
+    tag: "Featured",
+    category: "Necklaces",
+    date: 2021,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201014-2__40509_493x.jpg?v=1645116380",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/020200068-5__58390_360x.jpg?v=1645114243",
+    head: "Helio Charm Necklace",
+    price: 18,
+    tag: "Bestselling",
+    category: "Necklaces",
+    date: 2018,
+    availability: false,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201009-1__90403_493x.jpg?v=1645115936",
+  },
+  {
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201014-3__28860_360x.jpg?v=1645116382",
+    head: "Snake Chain Necklace",
+    price: 16,
+    tag: "New",
+    category: "Necklaces",
+    date: 2021,
+    availability: true,
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L201004-B-1__72807_493x.jpg?v=1645116088",
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800023-2__66753_493x.jpg?v=1645115925",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800023-3__70304_493x.jpg?v=1645115927",
+    head: "Singapore Chain Anklet Gold",
+    price: 23,
+    category: "Bracelets",
+    date: 2021,
+    tag: "Featured",
+    availability: false,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030700007-2__59557_493x.jpg?v=1645110218",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/crabs_model_1__49622_493x.jpg?v=1645110207",
+    head: "Maryland Crab Bracelet(18K)",
+    price: 18,
+    category: "Bracelets",
+    date: 2022,
+    tag: "Bestselling",
+    availability: true,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/686625FF-2__98081_493x.jpg?v=1645110189",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/D877C94B-4__63029_493x.jpg?v=1645110180",
+    head: "Brushed Sphere Cuff",
+    price: 23,
+    category: "Bracelets",
+    date: 2021,
+    tag: "New",
+    availability: true,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800011-2__07524_493x.jpg?v=1645115735",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800011-3__66808_493x.jpg?v=1645115737",
+    head: "Oval Rolo Chain Anklet",
+    price: 16,
+    category: "Bracelets",
+    date: 2018,
+    tag: "Featured",
+    availability: true,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030600088-B-1__23082_493x.jpg?v=1645115370",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030600088-B_2__22868_493x.jpg?v=1645115373",
+    head: "Blisfully Beaded Bracelet| Black(14K)",
+    price: 21,
+    category: "Bracelets",
+    date: 2022,
+    tag: "Bestselling",
+    availability: true,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030600088-S_2__57973_493x.jpg?v=1645115284",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/040300095_M__13624_493x.jpg?v=1645115285",
+    head: "Blissfully Beaded Bracelet | Crystal Clear(14K)",
+    price: 15,
+    category: "Bracelets",
+    date: 2022,
+    tag: "New",
+    availability: false,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030100135-2__46515_493x.jpg?v=1645114215",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030100135-1__67187_493x.jpg?v=1645114219",
+    head: "Always a Dreamer Bracelet(14K)",
+    price: 42,
+    category: "Bracelets",
+    date: 2018,
+    tag: "Featured",
+    availability: true,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030100130-1__59762_493x.jpg?v=1645112941",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030100130-3__84440_493x.jpg?v=1645112938",
+    head: "Beaming Bar Stone Bracelet| Silver",
+    price: 32,
+    category: "Bracelets",
+    date: 2022,
+    tag: "New",
+    availability: false,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800010-1__40868_493x.jpg?v=1645116239",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800010-3__96654_493x.jpg?v=1645116241",
+    head: "Disc Trio Anklet| Gold",
+    price: 17,
+    category: "Bracelets",
+    date: 2022,
+    tag: "Bestselling",
+    availability: true,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800020-G-1__18532_493x.jpg?v=1645115811",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800020-G-2__36689_493x.jpg?v=1645115815",
+    head: "Cool Girl Chain Anklet| Gold",
+    price: 18,
+    category: "Bracelets",
+    date: 2018,
+    tag: "Featured",
+    availability: true,
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030600088-P-1__93147_493x.jpg?v=1645115289",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030600088-P_M__75936_493x.jpg?v=1645115291",
+    head: "Blissfully Beaded Bracelet|Pearl",
+    price: 30,
+    category: "Bracelets",
+    date: 2022,
+    tag: "Featured",
+    availability: true,
+  },
 ];
 
 let returnAllData = () => {

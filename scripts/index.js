559c3349e6b7d2a0418d54938708853bb1afa953
sanajitjana <dev.sanajitjana@gmail.com>
2022-07-24 16:21:07 +0530

    home page click efect done

--- scripts/index.js
+++ scripts/index.js
@@ -52,68 +52,108 @@ login_icon.addEventListener("click", () => {
 
 let productData = [
   {
-    id: "h1",
-    img: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011100072-1_823x.jpg?v=1656343846",
-    hovImg:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011100072-2_533x.jpg?v=1656343845",
-    proName: "Crystal Clear Hook Earrings(14k) ",
-    price: "$14.00",
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-D-2_823x.jpg?v=1650308562",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-D-4_360x.jpg?v=1650308562",
+    head: "Textile Drop Earrings | Houndstooth",
+    price: 25,
+    category: "New",
+    date: 2018,
+    tag: "Bestselling",
+    availability: true,
+    id: 9,
   },
   {
-    id: "h2",
-    img: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101016-1_823x.jpg?v=1650308676",
-    hovImg:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101016-4_533x.jpg?v=1650308683",
-    proName: "Refined Stud Earrings",
-    price: "$21.00",
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800012-2__19471_493x.jpg?v=1645116158",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030800012-3__94518_493x.jpg?v=1645116160",
+    head: "Minimal Circle Rhinestone Anklet",
+    price: 19,
+    category: "bracelets",
+    date: 2018,
+    tag: "bestselling",
+    availability: true,
+    id: 10,
   },
   {
-    id: "h3",
-    img: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101061-1_823x.jpg?v=1650308599",
-    hovImg:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101061-4_533x.jpg?v=1650308606",
-    proName: "Terrace Drop Earrings ",
-    price: "$25.00",
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030100160-3__70321_493x.jpg?v=1645115635",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/030100160-3__31041_493x.jpg?v=1645115637",
+    head: "Thin and Thin Chain Bracelet(18K)",
+    price: 24,
+    category: "bracelets",
+    date: 2021,
+    tag: "Featured",
+    availability: false,
+    id: 11,
   },
   {
-    id: "h4",
-    img: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-D-1_823x.jpg?v=1650308558",
-    hovImg:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-D-4_533x.jpg?v=1650308562",
-    proName: "Textile Drop Earrings | Houndstooth",
-    price: "$25.00",
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011300002-2__23761_493x.jpg?v=1645110224",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/011300001-3__08167_360x.jpg?v=1645110216",
+    head: "Maryland Crab Earrings (18K & 24K)",
+    price: 18,
+    category: "earrings",
+    date: 2018,
+    tag: "bestselling",
+    availability: false,
+    id: 12,
   },
   {
-    id: "h5",
-    img: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-C-2_823x.jpg?v=1650308512",
-    hovImg:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-C-4_533x.jpg?v=1650308512",
-    proName: "Textile Drop Earrings | Checkered",
-    price: "$25.00",
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010200826-C-1__83187_493x.jpg?v=1652369565",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010200826-C-3__07211_360x.jpg?v=1652369565",
+    head: "Rhinestone Wing Earrings",
+    price: 14,
+    category: "earrings",
+    date: 2018,
+    tag: "bestselling",
+    availability: false,
+    id: 13,
   },
   {
-    id: "h6",
-    img: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-B-1_1100x.jpg?v=1650308456",
-    hovImg:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-B-4_533x.jpg?v=1650308459",
-    proName: "Textile Drop Earrings | Florals",
-    price: "$25.00",
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010100535-2__75509_493x.jpg?v=1645114230",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/010100535-4__11921_360x.jpg?v=1645114232",
+    head: "Boss Status Bar Earrings (14K)",
+    price: 16,
+    category: "earrings",
+    date: 2022,
+    tag: "Featured",
+    availability: false,
+    id: 14,
   },
   {
-    id: "h7",
-    img: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-A-2_823x.jpg?v=1650308396",
-    hovImg:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101009-A-4_533x.jpg?v=1650308396",
-    proName: "Textile Drop Earrings | Specks",
-    price: "$25.00",
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L401019-7-2_823x.jpg?v=1645728794",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L401019-7-3_823x.jpg?v=1645728794",
+    head: "Braided Ring",
+    price: 14,
+    category: "Rings",
+    date: 2022,
+    tag: "Bestselling",
+    availability: true,
+    id: 15,
   },
   {
-    id: "h8",
-    img: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101045-1_823x.jpg?v=1650308318",
-    hovImg:
-      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101045-4_533x.jpg?v=1650308327",
-    proName: "Jelly Hoop Earrings",
-    price: "$25.00",
+    image:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L401010-7-2__72676_823x.jpg?v=1645116401",
+    hovimage:
+      "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L401010-7-3__37928_823x.jpg?v=1645116399",
+    head: "Bridge Ring",
+    price: 14,
+    category: "Rings",
+    date: 2022,
+    tag: "Bestselling",
+    availability: true,
+    id: 16,
   },
 ];
 
@@ -153,6 +193,11 @@ let displayData = (data) => {
     let card = document.createElement("div");
     card.setAttribute("class", "card");
 
+    card.addEventListener("click", () => {
+      localStorage.setItem("productDetails", JSON.stringify(el));
+      window.location.href = "product-details.html";
+    });
+
     let love_btn = document.createElement("button");
     love_btn.setAttribute("class", "heart-back");
     love_btn.innerHTML = `<i class="fa fa-heart-o" aria-hidden="true"></i>`;
@@ -165,25 +210,25 @@ let displayData = (data) => {
     img_box.setAttribute("class", "img-box");
 
     let img = document.createElement("img");
-    img.src = el.img;
+    img.src = el.image;
     img.loading = "lazy";
     img_box.append(img);
 
     let title = document.createElement("a");
-    title.innerText = el.proName;
+    title.innerText = el.head;
     title.setAttribute("class", "prod-title");
 
     card.addEventListener("mouseover", (e) => {
-      img.src = el.hovImg;
+      img.src = el.hovimage;
       title.style.borderBottom = "1px solid #121212";
     });
     card.addEventListener("mouseout", (e) => {
-      img.src = el.img;
+      img.src = el.image;
       title.style.borderBottom = "1px solid transparent";
     });
 
     let price = document.createElement("p");
-    price.innerText = el.price;
+    price.innerText = `$${el.price.toFixed(2)}`;
     price.setAttribute("class", "prod-price");
 
     card.append(love_btn, img_box, title, price);
